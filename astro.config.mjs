a8ea97fe78303d56890c5c1efbea213febc432ff
kieran <56388994+kieranrossm@users.noreply.github.com>
2026-06-08 16:20:03 +0000

    Remove deprecated hybrid output config option

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import sanity from '@sanity/astro';
 import react from '@astrojs/react';
 
 export default defineConfig({
-  output: 'hybrid',
   integrations: [
     sanity({
       projectId: 'm8dqz3g',
